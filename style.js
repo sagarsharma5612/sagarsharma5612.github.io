b217f110d1ca923bbbb4515acb623e7b4f81f50c
Sagar Sharma <143262850+sagarsharma5612@users.noreply.github.com>
2023-12-27 20:48:47 +0530

    msg

--- style.js
+++ style.js
@@ -26,4 +26,16 @@ function closemenu() {
   sidemenu.style.right = "-200px";
 }
 
+			const scriptURL = 'https://script.google.com/macros/s/AKfycbzL1kd47rqJgzxm_cPlMrcaqc1PDtczbgGGeu9kZNNtAG8Cp3MA6WITRB89B0DokGgV/exec'
+			const form = document.forms['contact']
+		  
+			form.addEventListener('submit', e => {
+			  e.preventDefault()
+			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+				.then(response => alert("Thank you! your form is submitted successfully." ))
+				.then(() => {  window.location.reload(); })
+				.catch(error => console.error('Error!', error.message))
+			})
+	
+
 
