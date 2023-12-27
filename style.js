acaf8a45f0c22d6e0e637d526e108bfc84577bc5
Sagar Sharma <143262850+sagarsharma5612@users.noreply.github.com>
2023-12-27 20:25:24 +0530

    knjhb

--- style.js
+++ style.js
@@ -26,13 +26,4 @@ function closemenu() {
   sidemenu.style.right = "-200px";
 }
 
-			const scriptURL = "https://script.google.com/macros/s/AKfycbxueyTeKowCpgHVrVVJk1-tuDgkYBleGBGV8u_t1qzpA_jCN_QZ8nMiIYYppxTan2YAeA/exec"
-			const form = document.forms['form']
-		  
-			form.addEventListener('submit', e => {
-			  e.preventDefault()
-			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-				.then(response => alert("Thank you! your form is submitted successfully." ))
-				.then(() => {  window.location.reload(); })
-				.catch(error => console.error('Error!', error.message))
-			})
+
